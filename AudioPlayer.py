d280996c30b411d58cec019f28b8748c6dfa5f08
Clément Saintier <clement.saintier@altran.com>
2018-05-29 10:14:09 +0200

    Add untested feature: record ability in AudioPlayer

--- AudioPlayer.py
+++ AudioPlayer.py
@@ -7,8 +7,8 @@ Creation date: 2018-05-25
 
 Reference for style conventions : https://www.python.org/dev/peps/pep-0008/#naming-conventions
 """
-from PyQt5.QtCore import pyqtSlot, QUrl
-from PyQt5.QtMultimedia import QSoundEffect
+from PyQt5.QtCore import pyqtSlot, QUrl, QDateTime
+from PyQt5.QtMultimedia import QSoundEffect, QAudioRecorder, QAudioEncoderSettings, QMultimedia, QMediaRecorder
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout
 
 from Config import Config
@@ -24,11 +24,20 @@ class AudioPlayer(QWidget):
         self.sound_path = None
         self.init_ui()
         self.init_player()
+        if recordable:
+            self.init_recorder()
 
     def init_player(self):
         self.player = QSoundEffect()
         self.player.setVolume(1)
         self.player.playingChanged.connect(self.playing_changed_action)
+
+    def init_recorder(self):
+        self.recorder = QAudioRecorder()
+        audio_settings = QAudioEncoderSettings()
+        audio_settings.setCodec("audio/wav")
+        audio_settings.setQuality(QMultimedia.HighQuality)
+        self.recorder.setEncodingSettings(audio_settings)
 
     def init_ui(self):
         # Create widgets
@@ -39,7 +48,8 @@ class AudioPlayer(QWidget):
         if self.recordable:
             self.rec_button = ImageButton("images/record.png")
             self.rec_button.resize_image(Config.PLAYBACK_BUTTON_ICON_SIZE, Config.PLAYBACK_BUTTON_ICON_SIZE)
-            self.rec_button.clicked.connect(self.record)
+            self.rec_button.pressed.connect(self.start_recording)
+            self.rec_button.released.connect(self.stop_recording)
 
         # Setup layout for play and rec buttons
         self.buttons_layout = QVBoxLayout()
@@ -60,7 +70,7 @@ class AudioPlayer(QWidget):
             """)
 
     def play_sound(self):
-        assert (self.sound_path is not None, "Trying to play a sound but none were loaded in this AudioPlayer")
+        assert self.sound_path is not None, "Trying to play a sound but none were loaded in this AudioPlayer"
 
         self.play_button.setEnabled(False)
         self.player.play()
@@ -70,9 +80,18 @@ class AudioPlayer(QWidget):
         self.wave_display.load_audio(sound_path)
         self.player.setSource(QUrl.fromLocalFile(sound_path))
 
-    def record(self):
+    def start_recording(self):
+        print("recording")
         assert self.recordable
-        pass
+        datetime_stamp = QDateTime.currentDateTime().toString("yyyyMMdd_hhmmss")
+        self.recorder.setOutputLocation(QUrl.fromLocalFile("user_sounds/user_sound_" + datetime_stamp + ".wav"))
+        assert self.recorder.status() == QMediaRecorder.LoadedStatus, \
+            "Media Recorder not ready to record. Status :" + str(self.recorder.status())
+        self.recorder.record()
+
+    def stop_recording(self):
+        print("not recording")
+        self.recorder.stop()
 
     @pyqtSlot()
     def playing_changed_action(self):
